657dd032354c477ef98869fb2c0be99cfbc92bed
Aiman SIngh <145761115+Aimans30@users.noreply.github.com>
2024-03-05 11:22:30 +0530

    Add files via upload

---
+++ src/components/LikePost.jsx
@@ -0,0 +1,15 @@
+// LikePost.jsx
+import React from 'react';
+import Like from './Like';
+
+const LikePost = () => {
+  return (
+    <Like>
+      {({ likeCounter, handleLike }) => (
+        <button onClick={handleLike}>Like Post {likeCounter}</button>
+      )}
+    </Like>
+  );
+};
+
+export default LikePost;
